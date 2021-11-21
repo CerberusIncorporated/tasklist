6c6ec2ee5274a7bb29b9d479e0b4ae8cbec3a94b
cerberusincorporated <sancez.rick@gmail.com>
2021-11-21 18:29:58 +0200

    Create statistic controller and repository

---
+++ src/main/java/by/petproject/tasklist/backendspringboot/repo/StatRepository.java
@@ -0,0 +1,8 @@
+package by.petproject.tasklist.backendspringboot.repo;
+
+import by.petproject.tasklist.backendspringboot.entity.Stat;
+import org.springframework.data.repository.CrudRepository;
+
+public interface StatRepository extends CrudRepository<Stat, Long> {
+
+}
